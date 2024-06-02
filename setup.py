a617fcfdeacb9dc66b6d6687cfebc1bdf73db04a
Hui <summerlove66@163.com>
2024-06-03 02:58:28 +0800

    fix:crypto package changed

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     description="encrypt message or files for you",
     author="summerlove66",
     url="https://github.com/summerlove66/passbox.git",
-    py_modules=['passbox'],
-    install_requires=["click>=8.0.1",],
+    py_modules=['passbox',"aes_util"],
+    install_requires=["click>=8.0.1","pycryptodome>=3.20.0",'binaryornot'],
     python_requires=">=3.7",
     entry_points={
         'console_scripts': [
